52c59c6542e60ad73a55a816139ab5788d05acb7
ezekielmose <70557286+ezekielmose@users.noreply.github.com>
2025-02-25 13:28:43 +0300

    Update Strokes_models1.py

--- Strokes_models1.py
+++ Strokes_models1.py
@@ -37,27 +37,17 @@ def strock_predictor (input_data):
 def main():
     st.title ("Stroke Prediction Model")
     
-    gender = st.text_input("What is the Gender (0 - Female and 1 - male)")
-    age = st.text_input("Enter the age")
-    hypertension = st.text_input(" Hypertension 0 for -ve and 1 for +ve")
-    heart_disease = st.text_input(" Heart_disease 0 for has and 1 for not ")
-    ever_married = st.text_input(" Ever_married 0 for No and 1 for Yes")
-    work_type = st.text_input(" Work_type 0 for private and 1 for self employded, 2 for children, 3 for gov job, and 4 for Never_worked")
-    Residence_type = st.text_input ("Residence_type 0 for ubarn  and 1 for rural")
-    avg_glucose_level = ("Enter any value of (avg_glucose_level) as per the measurements")
-    bmi =st.text_input ("Enter any value of (BMI) as per the measurements")
-    smoking_status =st.text_input("Smoking_status 0 for never smoked, 1 for Unknown, 2 for formerly smoked, 3 for smokes")
+    gender = int (st.text_input("What is the Gender (0 - Female and 1 - male)"))
+    age = int (st.text_input("Enter the age"))
+    hypertension = int(st.text_input(" Hypertension 0 for -ve and 1 for +ve"))
+    heart_disease = int(st.text_input(" Heart_disease 0 for has and 1 for not "))
+    ever_married = int(st.text_input(" Ever_married 0 for No and 1 for Yes"))
+    work_type = int(st.text_input(" Work_type 0 for private and 1 for self employded, 2 for children, 3 for gov job, and 4 for Never_worked"))
+    Residence_type = int(st.text_input ("Residence_type 0 for ubarn  and 1 for rural"))
+    avg_glucose_level = int(("Enter any value of (avg_glucose_level) as per the measurements"))
+    bmi =int(st.text_input ("Enter any value of (BMI) as per the measurements"))
+    smoking_status = int(st.text_input("Smoking_status 0 for never smoked, 1 for Unknown, 2 for formerly smoked, 3 for smokes"))
     
-    
-    gender = pd.to_numeric(gender, errors='coerce')
-    age = pd.to_numeric(age, errors='coerce')
-    hypertension = pd.to_numeric(hypertension, errors='coerce')
-    heart_disease = pd.to_numeric(heart_disease, errors='coerce')
-    ever_married = pd.to_numeric(ever_married, errors='coerce')
-    Residence_type = pd.to_numeric(Residence_type, errors='coerce')
-    avg_glucose_level = pd.to_numeric(avg_glucose_level, errors='coerce')
-    bmi = pd.to_numeric(bmi, errors='coerce')
-    smoking_status = pd.to_numeric(smoking_status, errors='coerce')
     
     diagnosis1 = ""
     
